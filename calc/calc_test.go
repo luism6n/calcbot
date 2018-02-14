84bbba49e51f559c13fec00fc5a33345479ff0c4
Luís Möllmann <luism6n@gmail.com>
2018-02-14 15:31:56 -0200

    Refactor tests

--- calc/calc_test.go
+++ calc/calc_test.go
@@ -1,25 +1,39 @@
 package calc
 
 import (
-	"strconv"
 	"testing"
 )
 
 func TestLexer(t *testing.T) {
 	t.Run("Should recognize numbers", func(t *testing.T) {
-		for _, number := range []string{"1", "2", "999", "100001"} {
-			val, err := strconv.ParseFloat(number, 64)
-			if err != nil {
-				t.Fatalf("Something wrong with strconv.ParseFloat(%s, 64): %s", number, err.Error())
-			}
+		testCases := []struct {
+			Input string
+			Value float64
+		}{
+			{"1", 1.0},
+			{"2", 2.0},
+			{"999", 999.0},
+			{"100001", 100001.0},
+		}
 
-			lexer := newCalcLexer(number)
+		for _, c := range testCases {
+			lexer := newCalcLexer(c.Input)
 			lval := &yySymType{}
 			token := lexer.Lex(lval)
 
-			if token != NUMBER || lval.val != val {
-				t.Fatalf("%d != NUMBER or  %f != %f", token, lval.val, val)
+			if token != NUMBER || !floatEquals(lval.val, c.Value, 0.000001) {
+				t.Fatalf("%d != %d or  %f != %f", token, NUMBER, lval.val, c.Value)
 			}
 		}
 	})
+}
+
+func floatEquals(a, b, eps float64) bool {
+	if a >= b && a-b < eps {
+		return true
+	} else if a < b && b-a < eps {
+		return true
+	}
+
+	return false
 }
